5ac45602a9666b1d2850b90cdb07af1a2fda85b5
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2023-01-06 13:47:27 +0100

    adding VM.PublicIP and NSG rules

--- infra/modules/spoke/vm/main.tf
+++ infra/modules/spoke/vm/main.tf
@@ -1,3 +1,11 @@
+resource "azurerm_public_ip" "vm_ip" {
+  name                = "${var.prefix}-vm-ip"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_network_interface" "vm_nic" {
   name                = "${var.prefix}-nic"
   location            = var.location
@@ -7,6 +15,7 @@ resource "azurerm_network_interface" "vm_nic" {
     name                          = "${var.prefix}-ip"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_ip.id
   }
 }
 
--- infra/modules/spoke/vnet/main.tf
+++ infra/modules/spoke/vnet/main.tf
@@ -2,11 +2,33 @@ locals {
   vnet_name = "${var.prefix}-vnet"
 }
 
+#################################################
+#      NSG
+#################################################
+
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.prefix}-nsg"
   location            = var.location
   resource_group_name = var.resource_group_name
 }
+
+resource "azurerm_network_security_rule" "nsg-ssh" {
+  name                        = "allow-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+#################################################
+#      VNET
+#################################################
 
 resource "azurerm_virtual_network" "vnet" {
   name                = local.vnet_name
