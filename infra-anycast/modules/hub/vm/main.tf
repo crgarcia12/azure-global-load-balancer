d6e868d9135afe62d90c83442f9a22c36403306b
Federico Guerrini <fguerri@users.noreply.github.com>
2023-01-16 18:10:53 +0100

    Update main.tf
    Enabled IP Forwarding on BGP VM

--- infra-anycast/modules/hub/vm/main.tf
+++ infra-anycast/modules/hub/vm/main.tf
@@ -10,7 +10,8 @@ resource "azurerm_network_interface" "vm_nic" {
   name                = "${var.prefix}-vm-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
-
+  
+  enable_ip_forwarding            = true
   ip_configuration {
     name                          = "${var.prefix}-ip"
     subnet_id                     = var.subnet_id
