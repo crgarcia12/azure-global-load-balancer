5a806bdfceb61378ee5a0ec367e8f81d89a3de21
Federico Guerrini <fguerri@users.noreply.github.com>
2023-01-16 17:13:15 +0100

    Update main.tf
    Added ARS peering configuration

--- infra-anycast/modules/hub/routeserver/main.tf
+++ infra-anycast/modules/hub/routeserver/main.tf
@@ -16,3 +16,10 @@ resource "azurerm_route_server" "ars" {
   subnet_id                        = var.subnet_id
   branch_to_branch_traffic_enabled = true
 }
+
+resource "azurerm_route_server_bgp_connection" "eastus-vm1-bgpconnection" {
+  name            = "eastus-vm1-bgpconnection"
+  route_server_id = azurerm_route_server.ars.id
+  peer_asn        = 65111
+  peer_ip         = "10.200.2.4"
+}
