5e87dc09b98bfd4aa66e80fe844a8dce9f5fea78
fguerri <fguerri@microsoft.com>
2023-01-19 16:01:18 +0100

    Added fw_vip

--- infra-anycast/modules/hub/outputs.tf
+++ infra-anycast/modules/hub/outputs.tf
@@ -6,6 +6,10 @@ output "hub_vnet_name" {
   value = module.hub_vnet.vnet_name
 }
 
+output "fw_vip" {
+  value = module.hub_fw.fw_vip
+}
+
 output "hub_rg_name" {
   value = azurerm_resource_group.hub_rg.name
 }
