d28c137a1ea3bff84652ef4dda27d4c92e589166
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2023-01-20 12:02:13 +0100

    changing octets for easier reading

--- infra-anycast/main.tf
+++ infra-anycast/main.tf
@@ -28,7 +28,7 @@ module "hub" {
   source          = "./modules/hub"
   prefix          = "${var.prefix}-eus-hub"
   location        = "eastus"
-  ip_second_octet = "200"
+  ip_second_octet = "222"
 }
 
 #################################
@@ -38,7 +38,7 @@ module "spoke_weu" {
   source          = "./modules/spoke"
   prefix          = "${var.prefix}-eus-s1"
   location        = "eastus"
-  ip_second_octet = "210"
+  ip_second_octet = "223"
   hub_vnet_name   = module.hub.hub_vnet_name
   hub_vnet_id     = module.hub.hub_vnet_id
   hub_rg_name     = module.hub.hub_rg_name
@@ -57,7 +57,7 @@ module "hub_weu" {
   source          = "./modules/hub"
   prefix          = "${var.prefix}-weu-hub"
   location        = "westeurope"
-  ip_second_octet = "100"
+  ip_second_octet = "111"
 
 }
 
@@ -68,7 +68,7 @@ module "spoke_weu_s1" {
   source                  = "./modules/spoke"
   prefix                  = "${var.prefix}-weu-s1"
   location                = "westeurope"
-  ip_second_octet         = "110"
+  ip_second_octet         = "112"
   hub_vnet_name           = module.hub_weu.hub_vnet_name
   hub_vnet_id             = module.hub_weu.hub_vnet_id
   hub_rg_name             = module.hub_weu.hub_rg_name
